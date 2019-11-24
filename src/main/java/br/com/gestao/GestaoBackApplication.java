e8fefb6c892f72004df2a3a7da72494b80687b5e
Willian Vagner Vicente Correa <willian.vag@gmail.com>
2019-11-24 01:23:35 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/gestao/GestaoBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoBackApplication.class, args);
+    }
+
+}
